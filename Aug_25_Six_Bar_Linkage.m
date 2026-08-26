330a9bb45952e55ea43327d105292c79a7c421ea
alibragg <alipbragg@icloud.com>
2026-08-26 19:38:19 -0400

    lecture 8/26 code

--- Aug_25_Six_Bar_Linkage.m
+++ Aug_25_Six_Bar_Linkage.m
@@ -139,3 +139,122 @@ disp('Force G:');
 disp([Force_Gx, Force_Gy]);
 disp('Input Torque:');
 disp(Input_Torque);
+
+% Loop ABCDA
+
+syms wBEC wCD
+omega_AB = [0 0 1];
+omega_BEC = [0 0 wBEC];
+omega_CD = [0 0 wCD];
+
+eqn11 = cross(omega_AB, B-A) + cross(omega_BEC, C-B) + cross(omega_CD, D-C) == 0;
+
+loop1Solution = solve(eqn11, [wBEC wCD]);
+
+% Extract angular velocities from the loop solution
+angularVelocity_BEC = double(loop1Solution.wBEC);
+angularVelocity_CD = double(loop1Solution.wCD);
+
+disp('angularVelocity_BEC')
+disp(angularVelocity_BEC)
+disp('angularVelocity_CD')
+disp(angularVelocity_CD)
+
+omegaBEC = [0 0 angularVelocity_BEC];
+omegaCD = [0 0 angularVelocity_CD];
+
+% Second Loop
+% DCEFGD
+
+syms wEF wFG
+omega_EF = [0 0 wEF];
+omega_FG = [0 0 wFG];
+
+eqn12 = cross(omegaCD, C-D) + cross(omegaBEC, E-C) + cross(omega_EF, F-E) + cross(omega_FG, G-F) == 0;
+
+% Solve the second loop equations
+loop2Solution = solve(eqn12, [wEF wFG]);
+
+% Extract angular velocities from the second loop solution
+angularVelocity_EF = double(loop2Solution.wEF);
+angularVelocity_FG = double(loop2Solution.wFG);
+
+% Angular Acceleration
+% Loop 1 ABCDA
+% Angular Acceleration for Loop 1
+syms aBEC aCD
+alpha_AB = [0 0 0]; % Assuming no angular acceleration for AB
+alpha_BEC = [0 0 aBEC];
+alpha_CD = [0 0 aCD];
+
+a_B_A = cross(alpha_AB, B-A) + cross(omega_AB, cross(omega_AB, B-A)); % accel of B w.r.t A
+a_C_B = cross(alpha_BEC, C-B) + cross(omegaBEC, cross(omegaBEC, C-B));
+a_D_C = cross(alpha_CD, D-C) + cross(omegaCD, cross(omegaCD, D-C));
+
+eqn13 = a_B_A + a_C_B + a_D_C == 0;
+
+loop1AccSolution = solve(eqn13, [aBEC aCD]);
+
+alphaBEC = double(loop1AccSolution.aBEC);
+alphaCD = double(loop1AccSolution.aCD);
+
+alphaBEC_vector = [0 0 alphaBEC];
+alphaCD_vector = [0 0 alphaCD];
+
+disp('alphaBEC')
+disp(alphaBEC)
+disp('alphaCD')
+disp(alphaCD)
+
+% Loop 2 DCEFGD
+
+syms aEF aFG
+alpha_EF = [0 0 aEF];
+alpha_FG = [0 0 aFG];
+
+% a_C_D + a_E_C + a_F_E + a_G_F = 0
+a_C_D = cross(alphaCD_vector, C-D) + cross(omegaCD, cross(omegaCD, C-D));
+a_E_C = cross(alphaBEC_vector, E-C) + cross(omegaBEC, cross(omegaBEC, E-C));
+
+angVel_EF = [0 0 angularVelocity_EF];
+angVel_FG = [0 0 angularVelocity_FG];
+
+a_F_E = cross(alpha_EF, F-E) + cross(angVel_EF, cross(angVel_EF, F-E));
+a_G_F = cross(alpha_FG, G-F) + cross(angVel_FG, cross(angVel_FG, G-F));
+
+eqn14 = a_C_D + a_E_C + a_F_E + a_G_F == 0;
+
+loop2AccSolution = solve(eqn14, [aEF aFG]);
+
+% Extract angular accelerations from the loop solution
+alphaEF = double(loop2AccSolution.aEF)
+alphaFG = double(loop2AccSolution.aFG)
+
+disp('alphaEF')
+disp(alphaEF)
+disp('alphaFG')
+disp(alphaFG)
+
+% Extract angular accelerations for the second loop
+alphaEF = double(loop2AccSolution.aEF);
+alphaFG = double(loop2AccSolution.aFG);
+
+
+% Velocity at Joint
+
+vB_A = cross(omega_AB, B-A);
+
+% VE_A = V_E_B + V_B_A;
+
+v_E_B = cross(omegaBEC, E-B);
+
+vE_A = v_E_B + vB_A;
+
+% V_S4/G = V_S4_F + V_F_G (center of mass)
+V_S4_F = cross(angVel_EF, S4-F);
+
+V_F_G = cross(angVel_FG, F-G);
+
+vS4_G = V_S4_F + V_F_G;
+
+%test
