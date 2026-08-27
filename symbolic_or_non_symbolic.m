f86a8e94bc5646b0f23158b070b3d5cd7b0843ba
alibragg <alipbragg@icloud.com>
2026-08-26 20:01:00 -0400

    finished code

--- symbolic_or_non_symbolic.m
+++ symbolic_or_non_symbolic.m
@@ -1,3 +1,14 @@
 % Show an example of solving two simultaneous equations using 
 % MATLAB (symbolic method or non-symbolic method)
 
+syms x y
+
+eq1 = 2*x + 3*y == 6;
+eq2 = 4*x - y == 5;
+
+solution = solve([eq1, eq2], [x, y]);
+
+x_sol = solution.x;
+y_sol = solution.y;
+
+fprintf('X = %d, Y = %d\n', x_sol, y_sol)
